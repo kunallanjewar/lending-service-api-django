88a69a25d6271af0656529541f601cdf02ed6f2a
Kunal Lanjewar <kunallanjewar@gmail.com>
2018-06-14 16:58:19 -0700

    add creat method to view

--- lendingservice/api/views.py
+++ lendingservice/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import Serializer
+from .models import User
 
-# Create your views here.
+class CreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = Serializer()
+
+    def create(self, serializer):
+        serializer.save()
