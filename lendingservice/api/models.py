54b7ba60549e7c5cdc8b770fb9be384198eb3a8e
Kunal Lanjewar <kunal.lanjewr@gmail.com>
2018-06-14 11:09:32 -0700

    init model and test it. TDD

--- lendingservice/api/models.py
+++ lendingservice/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class LendingRecord(models.Model):
+    first_name = models.CharField(max_length=255, blank=False)
+    last_name = models.CharField(max_length=255, blank=False)
+    dob = models.DateField(auto_now=False, auto_now_add=False)
+    credit_line = models.DecimalField(max_digits=5, decimal_places=2)
+    balance = models.DecimalField(max_digits=5, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.first_name)
--- lendingservice/api/tests.py
+++ lendingservice/api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import LendingRecord
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    """Class defines test suite for out model"""
+
+    def set_up(self):
+        self.name = "lending record"
+        self.lending_record = LendingRecord(name=self.name)
+
+    def test_model_can_create_a_table_in_db(self):
+        old_count = LendingRecord.objects.count()
+        self.lending_record.save()
+        new_count = LendingRecord.objects.count()
+        self.assertNotEqual(old_count, new_count)
