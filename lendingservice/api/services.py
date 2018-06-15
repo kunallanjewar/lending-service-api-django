fbf44c2c8e638b6c9b74fa32f28453d9f4fbc494
Kunal Lanjewar <kunallanjewar@gmail.com>
2018-06-15 15:22:12 -0700

    update services and view

--- lendingservice/api/services.py
+++ lendingservice/api/services.py
@@ -4,9 +4,9 @@ class LendingService(object):
     @staticmethod
     def create_user(self, first_name, last_name, email):
         self.user_data = User(
-            self.first_name = first_name
-            self.last_name = last_name
-            self.email = email
+            first_name = self.first_name,
+            last_name = self.last_name,
+            email = self.email
         )
 
     @staticmethod
--- lendingservice/api/views.py
+++ lendingservice/api/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics, status
+from rest_framework.response import Response
 from .serializers import *
 from .models import *
+from .services import *
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     def perform_create(self, serializer):
-        serializer.save()
+        if serializer.is_valid():
+            lending_service = LendingService()
+            serializer.save()
+        return Response(serializer.data)
 
 class OpenAccountView(generics.CreateAPIView):
     queryset = AccountDetails.objects.all()
