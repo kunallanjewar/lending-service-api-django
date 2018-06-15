ab83f9403cdbbcc7c2997d1c17eeb5f35af692dd
Kunal Lanjewar <kunallanjewar@gmail.com>
2018-06-14 18:09:39 -0700

    fix test suite: user not found error

--- lendingservice/api/tests.py
+++ lendingservice/api/tests.py
@@ -2,19 +2,19 @@ from django.test import TestCase
 from .models import User
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 class ModelTestCase(TestCase):
-    """Class defines test suite for out model"""
+    """Class defines test suite for our model"""
 
     def setUp(self):
         self.first_name = "kunal"
         self.user = User(first_name=self.first_name)
 
     def test_model_can_create_a_table_in_db(self):
-        old_count = user.objects.count()
+        old_count = User.objects.count()
         self.user.save()
-        new_count = user.objects.count()
+        new_count = User.objects.count()
         self.assertNotEqual(old_count, new_count)
 
 class ViewTestCase(TestCase):
