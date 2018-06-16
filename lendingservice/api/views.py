159a480c5cf16d5dd6ed9f550ffab584e6570d39
Kunal Lanjewar <kunallanjewar@gmail.com>
2018-06-15 19:04:53 -0700

    add viewset

--- lendingservice/api/views.py
+++ lendingservice/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets
+from rest_framework.routers import DefaultRouter
 from rest_framework.response import Response
 from .serializers import *
 from .models import *
@@ -27,6 +28,27 @@ class WithdrawView(generics.ListAPIView):
 class MakePaymentView(generics.ListCreateAPIView):
     pass
 
-class TransactionsHistoryView(generics.ListAPIView):
-    queryset = Transactions.objects.all()
-    serializer_class = TransactionsSerializer
+class TransactionsHistoryView(viewsets.ViewSet):
+
+    def retrieve(self, request, pk=None):
+        queryset = Transactions.objects.all()
+        transactions = get_object_or_404(queryset, pk=pk)
+        serializer = TransactionsSerializer(transactions)
+        return Response(serializer.data)
+
+transactions_list = TransactionsHistoryView.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+details = TransactionsHistoryView.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+router = DefaultRouter()
+router.register(r'^api/transactions/$',
+                                TransactionsHistoryView,
+                                base_name='transactions'
+                            )
