6119cb12903d3135ddf6332cfbf215ce6dddf26a
Kunal Lanjewar <kunallanjewar@gmail.com>
2018-06-18 16:29:52 -0700

    make apr fixed

--- lendingservice/api/services.py
+++ lendingservice/api/services.py
@@ -4,26 +4,19 @@ class LendingService(object):
     def __init__(self,
                 credit_min=500,
                 credit_max=5000,
-                apr_min=0.05,
-                apr_max=0.35):
+                apr=0.35):
 
         self.credit_min = credit_min
         self.credit_max = credit_max
-        self.apr_min = apr_min
-        self.apr_max = apr_max
+        self.apr = apr
 
     @property
-    def apr(self):
-        return self.apr_max
+    def apr_fixed(self):
+        return self.apr
 
     @property
     def credit_line(self):
         return self.credit_max
-
-    @property
-    def random_apr(self):
-        import random
-        return (random.randrange(apr_min*100, apr_max*100))/100
 
     @property
     def random_credit_line(self):
